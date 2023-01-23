906ddc66580e7bde7718f2c04c5bdf86fe25eee5
sleynsol <sleyn_sol@protonmail.com>
2023-01-23 15:19:46 +0100

    Initial project with program

---
+++ programs/msg3-program/src/lib.rs
@@ -0,0 +1,382 @@
+use anchor_lang::prelude::*;
+
+
+declare_id!("DmNoHc7VCXintCLAskTeK3jDuD9sC5XkBpYAdkSbGg2e");
+
+#[program]
+pub mod msg3_program {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let global = &mut ctx.accounts.global;
+
+        global.global_post_count = 0;
+        global.global_user_count = 0;
+        global.category_count = 0;
+        global.last_category_pda = ctx.program_id.key();
+        global.last_user = ctx.program_id.key();
+        global.bump = *ctx.bumps.get("global").unwrap();
+        Ok(())
+    }
+
+
+    pub fn create_category(ctx: Context<CreateCategory>, name: String) -> Result<()> {
+        let global = &mut ctx.accounts.global;
+        let category = &mut ctx.accounts.category;
+
+        
+        if name.as_bytes().len() < 1 {
+            panic!("Name too short, increase to 1.")
+        }
+
+        if name.as_bytes().len() > 20 {
+            panic!("Name too long, reduce to 20.")
+        }
+
+        category.name = name;
+        category.creator = ctx.accounts.creator.key();
+        category.post_count = 0;
+        category.predecessor = global.last_category_pda;
+        category.bump = *ctx.bumps.get("category").unwrap();
+
+        global.category_count += 1;
+        global.last_category_pda = category.key();
+
+        Ok(())
+    }
+
+    pub fn post(ctx: Context<CreatePost>, text: String) -> Result<()> {
+        let global = &mut ctx.accounts.global;
+        let category = &mut ctx.accounts.category;
+        let post = &mut ctx.accounts.post;
+        let user = &mut ctx.accounts.user;
+
+        if text.as_bytes().len() > 200 {
+            panic!("Text too long, reduce to 200.")
+        }
+
+        post.text = text;
+        post.likes = 0;
+        post.predecessor = category.last_post_pda;
+        post.writer = ctx.accounts.creator.key();
+        post.bump = *ctx.bumps.get("post").unwrap();
+
+        category.last_post_pda = post.key();
+
+        user.post_count += 1;
+        category.post_count += 1;
+        global.global_post_count += 1;
+
+        Ok(())
+    }
+
+    pub fn comment(ctx: Context<CreateComment>, text: String) -> Result<()> {
+        let post = &mut ctx.accounts.post;
+        let comment = &mut ctx.accounts.comment;
+        let user = &mut ctx.accounts.user;
+
+        if text.as_bytes().len() > 200 {
+            panic!("Text too long, reduce to 200.")
+        }
+
+        comment.text = text;
+        comment.likes = 0;
+        comment.predecessor = post.last_comment_pda;
+        comment.writer = ctx.accounts.creator.key();
+        comment.bump = *ctx.bumps.get("comment").unwrap();
+
+        post.last_comment_pda = comment.key();
+
+        post.comments += 1;
+        user.comment_count += 1;
+        
+        Ok(())
+    }
+
+
+    pub fn like_post(ctx: Context<LikePost>) -> Result<()> {
+        let post = &mut ctx.accounts.post;
+        let like = &mut ctx.accounts.like;
+
+        post.likes += 1;
+        like.bump = *ctx.bumps.get("like").unwrap();
+
+        Ok(())
+    }
+
+    pub fn like_comment(ctx: Context<LikeComment>) -> Result<()> {
+        let comment = &mut ctx.accounts.comment;
+        let like = &mut ctx.accounts.like;
+
+        comment.likes += 1;
+        like.bump = *ctx.bumps.get("like").unwrap();
+
+        Ok(())
+    }
+
+
+
+    pub fn create_user(ctx: Context<CreateUser>) -> Result<()> {
+        let global = &mut ctx.accounts.global;
+        let creator = &mut ctx.accounts.creator;
+        let user = &mut ctx.accounts.user;
+
+        user.pubkey = creator.key();
+        user.post_count = 0;
+        user.predecessor = global.last_user;
+        user.bump = *ctx.bumps.get("user").unwrap();
+
+        global.last_user = user.key();
+        global.global_user_count += 1;
+
+        Ok(())
+    }
+
+}
+
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + GlobalStats::MAX_SIZE, seeds = [b"global"], bump
+    )]
+    pub global: Account<'info, GlobalStats>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct CreateCategory<'info> {
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Category::MAX_SIZE, seeds = [b"category", name.as_bytes()], bump
+    )]
+    pub category: Account<'info, Category>,
+
+    #[account(
+        mut,
+        seeds = [b"global"], bump = global.bump
+    )]
+    pub global: Account<'info, GlobalStats>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct CreatePost<'info> {
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Post::MAX_SIZE, seeds = [b"post", user.key().as_ref(), &user.post_count.to_be_bytes()], bump
+    )]
+    pub post: Account<'info, Post>,
+
+    #[account(
+        mut,
+        seeds = [b"user", creator.key().as_ref()], bump = user.bump
+    )]
+    pub user: Account<'info, User>,
+
+    #[account(
+        mut,
+        seeds = [b"category", category.name.as_bytes()], bump = category.bump
+    )]
+    pub category: Account<'info, Category>,
+
+    #[account(
+        mut,
+        seeds = [b"global"], bump = global.bump
+    )]
+    pub global: Account<'info, GlobalStats>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct LikePost<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Like::MAX_SIZE, seeds = [b"like", user.key().as_ref(), post.key().as_ref()], bump
+    )]
+    pub like: Account<'info, Like>,
+
+
+    #[account(
+        mut,
+        seeds = [b"user", creator.key().as_ref()], bump = user.bump
+    )]
+    pub user: Account<'info, User>,
+
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+
+    pub system_program: Program<'info, System>
+}
+
+
+#[derive(Accounts)]
+pub struct LikeComment<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Like::MAX_SIZE, seeds = [b"like", user.key().as_ref(), comment.key().as_ref()], bump
+    )]
+    pub like: Account<'info, Like>,
+
+
+    #[account(
+        mut,
+        seeds = [b"user", creator.key().as_ref()], bump = user.bump
+    )]
+    pub user: Account<'info, User>,
+
+    #[account(mut)]
+    pub comment: Account<'info, Comment>,
+
+    pub system_program: Program<'info, System>
+}
+
+
+#[derive(Accounts)]
+pub struct CreateComment<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Comment::MAX_SIZE, seeds = [b"comment", user.key().as_ref(), &user.comment_count.to_be_bytes()], bump
+    )]
+    pub comment: Account<'info, Comment>,
+
+    #[account(
+        mut,
+        seeds = [b"user", creator.key().as_ref()], bump = user.bump
+    )]
+    pub user: Account<'info, User>,
+
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct CreateUser<'info> {
+    
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + User::MAX_SIZE, seeds = [b"user", creator.key().as_ref()], bump
+    )]
+    pub user: Account<'info, User>,
+
+    #[account(
+        mut,
+        seeds = [b"global"], bump = global.bump
+    )]
+    pub global: Account<'info, GlobalStats>,
+    pub system_program: Program<'info, System>
+}
+
+
+#[account]
+pub struct GlobalStats {
+    global_post_count: u128,
+    global_user_count: u128,
+    category_count: u64,
+    last_user: Pubkey,
+    last_category_pda: Pubkey,
+    bump: u8
+}
+
+impl GlobalStats {
+    pub const MAX_SIZE: usize = 16 + 16 + 8 + 32 + 32 + 1;
+}
+
+#[account]
+pub struct User {
+    post_count: u64,
+    comment_count: u64,
+    predecessor: Pubkey,
+    pubkey: Pubkey,
+    bump: u8
+}
+
+impl User {
+    pub const MAX_SIZE: usize = 8 + 8 + 32 + 32 + 1;
+}
+
+#[account]
+pub struct Category {
+    name: String,
+    post_count: u64,
+    last_post_pda: Pubkey,
+    predecessor: Pubkey,
+    creator: Pubkey,
+    bump: u8
+}
+
+impl Category {
+    pub const MAX_SIZE: usize = (4 + 20) + 8 + 32 + 32 + 32 + 1;
+}
+
+#[account]
+pub struct Post {
+    text: String,
+    likes: u64,
+    comments: u64,
+    last_comment_pda: Pubkey,
+    predecessor: Pubkey,
+    writer: Pubkey,
+    bump: u8
+}
+
+impl Post {
+    pub const MAX_SIZE: usize = (4 + 200) + 8 + 8 + 32 + 32 + 32 + 1;
+}
+
+#[account]
+pub struct Like {
+    bump: u8
+}
+
+impl Like {
+    pub const MAX_SIZE: usize = 1;
+}
+
+#[account]
+pub struct Comment {
+    text: String,
+    likes: u64,
+    predecessor: Pubkey,
+    writer: Pubkey,
+    bump: u8
+}
+
+impl Comment {
+    pub const MAX_SIZE: usize = (4 + 200) + 8 + 32 + 32 + 1;
+}
---
+++ tests/msg3-program.ts
@@ -0,0 +1,168 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { Msg3Program } from "../target/types/msg3_program";
+import { BN } from "bn.js";
+
+
+describe("msg3-program", () => {
+
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.local("https://api.testnet.solana.com")
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Msg3Program as Program<Msg3Program>;
+  const programId = new PublicKey("DmNoHc7VCXintCLAskTeK3jDuD9sC5XkBpYAdkSbGg2e");
+
+  it("shall initiailize", async() => {
+
+    const [globalPDA, globalBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("global")
+      ],
+      programId)
+
+    await program.methods.initialize()
+    .accounts({
+        creator: provider.wallet.publicKey,
+        global: globalPDA
+      }).rpc()
+
+      let globalStats = await program.account.globalStats.fetch(globalPDA)
+
+      console.log("Initialized!")
+      console.log(globalStats)
+
+  })
+
+  it("Create global category", async () => {
+
+    const [globalStatsPDA, globalBump] = await PublicKey.findProgramAddress(
+    [
+      anchor.utils.bytes.utf8.encode("global")
+    ],
+    programId)
+
+    const [globalCategoryPDA, globalCategoryBump] = await PublicKey.findProgramAddress(
+    [
+      anchor.utils.bytes.utf8.encode("category"),
+      anchor.utils.bytes.utf8.encode("global")
+    ],
+    programId)
+
+    console.log("global stats: ", globalStatsPDA.toString())
+    console.log("global category: ", globalCategoryPDA.toString())
+
+    await program.methods.createCategory("global")
+    .accounts({
+      creator: provider.wallet.publicKey,
+      category: globalCategoryPDA,
+      global: globalStatsPDA,
+    }).rpc()
+
+    console.log("Created global category")
+
+    let globalCategory = await program.account.category.fetch(globalCategoryPDA)
+
+    console.log(globalCategory)
+
+  });
+
+  it("Shall create a post",async () => {
+    
+    const [globalStatsPDA, globalBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("global")
+      ],
+      programId
+    )
+  
+    const [globalCategoryPDA, globalCategoryBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("category"),
+        anchor.utils.bytes.utf8.encode("global")
+      ],
+      programId
+    )
+
+    let globalCategory = await program.account.category.fetch(globalCategoryPDA)
+
+    console.log("POST COUNT: ", globalCategory.postCount.toNumber())
+    
+    const [postPDA, pdaBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("post"),
+        anchor.utils.bytes.utf8.encode("global"),
+        Uint8Array.of(...globalCategory.postCount.toArray('be', 8))
+      ],
+      programId
+    )
+
+    console.log("POST COUNT U8: ", Uint8Array.of(...globalCategory.postCount.toArray('be', 8)))
+
+    console.log("POST PDA ", postPDA.toString())
+
+      await program.methods.post("This is the first Post!")
+      .accounts({
+        creator: provider.wallet.publicKey,
+        category: globalCategoryPDA,
+        post: postPDA,
+        global: globalStatsPDA
+      }).rpc()
+      
+      let post = await program.account.post.fetch(postPDA)
+
+      console.log("Created post")
+      console.log(post)
+
+  })
+
+  it('shall fetch all accounts',async () => {
+
+    let postCounts = [new BN(0), new BN(1), new BN(2)]
+
+    for(let count of postCounts) {
+      const [postPDA, pdaBump] = await PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("post"),
+          anchor.utils.bytes.utf8.encode("global"),
+          Uint8Array.of(...count.toArray('be', 8))
+        ],
+        programId
+      )
+
+      let post = await program.account.post.fetch(postPDA)
+
+      console.log("POST ", count.toNumber())
+      console.log("POST PDA", postPDA.toString())
+      console.log(post)
+    }
+
+
+    const [globalStatsPDA, globalBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("global")
+      ],
+      programId
+    )
+  
+    const [globalCategoryPDA, globalCategoryBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("category"),
+        anchor.utils.bytes.utf8.encode("global")
+      ],
+      programId
+    )
+    
+    let globalStats = await program.account.globalStats.fetch(globalStatsPDA)
+    let category = await program.account.category.fetch(globalCategoryPDA)
+    
+
+    console.log("STATS: ", globalStats)
+    console.log("CATEGORY: ", category)
+
+
+  })
+});
